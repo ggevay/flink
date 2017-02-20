1c70ae098db4153618e5aff04f245125c9fc1606
Gabor Gevay <ggab90@gmail.com>
2017-02-20 11:46:48 +0100

    stuff

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/SingleOutputStreamOperator.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/SingleOutputStreamOperator.java
@@ -408,7 +408,7 @@ public class SingleOutputStreamOperator<T> extends DataStream<T> {
 	// ------------------------------------------------------------------------
 
 	@Override
-	protected DataStream<T> setConnectionType(StreamPartitioner<T> partitioner) {
+	public DataStream<T> setConnectionType(StreamPartitioner<T> partitioner) {
 		return new SingleOutputStreamOperator<>(this.getExecutionEnvironment(), new PartitionTransformation<>(this.getTransformation(), partitioner));
 	}
 
