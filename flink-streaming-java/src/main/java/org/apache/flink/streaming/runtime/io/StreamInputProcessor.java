f036c253ca9ba4519b38e6cb10adc6f2e6f7b424
Gabor Gevay <ggab90@gmail.com>
2017-06-19 20:15:47 +0200

    Add NoAutoClose

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/StreamInputProcessor.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/StreamInputProcessor.java
@@ -45,6 +45,7 @@ import org.apache.flink.runtime.plugable.NonReusingDeserializationDelegate;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
 import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.runtime.gg.NoAutoClose;
 import org.apache.flink.streaming.runtime.streamrecord.StreamElement;
 import org.apache.flink.streaming.runtime.streamrecord.StreamElementSerializer;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
@@ -226,11 +227,16 @@ public class StreamInputProcessor<IN> {
 				}
 			}
 			else {
-				isFinished = true;
-				if (!barrierHandler.isEmpty()) {
-					throw new IllegalStateException("Trailing data in checkpoint barrier handler.");
+				if (!(streamOperator instanceof NoAutoClose)) {
+					isFinished = true;
+					if (!barrierHandler.isEmpty()) {
+						throw new IllegalStateException("Trailing data in checkpoint barrier handler.");
+					}
+					return false;
+				} else {
+					Thread.sleep(100);
+					return true;
 				}
-				return false;
 			}
 		}
 	}
